49343e4c9e2e6d1d2576fe3192be6207428a0100
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-02-17 22:18:06 +0530

    implemented DeleteQuery

---
+++ src/DeleteData.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteData {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/college_db";
+        String username = "root";
+        String password = "0R4cl34321_";
+
+        try{
+            Connection con = DriverManager.getConnection(url,username,password);
+            System.out.println("connected");
+
+            String deleteQuery = "DELETE FROM teachers WHERE name = ?";
+            PreparedStatement ps = con.prepareStatement(deleteQuery);
+
+            ps.setString(1,"souvik");
+
+            ps.executeUpdate();
+            System.out.println("successfully deleted");
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
